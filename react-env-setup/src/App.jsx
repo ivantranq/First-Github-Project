33fc068896bb7aa411854a5cfeb0b38f5f804112
Ivan Tranquilan <ivan.tranquilan@outlook.com>
2022-07-21 11:10:20 +0930

    react setup

---
+++ react-env-setup/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './components/nav.jsx'
+import Header from './components/header.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Nav></Nav>
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
