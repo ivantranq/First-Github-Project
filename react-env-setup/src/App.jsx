3d2d2b02acb72bcb8966cf1f2ca0fb6072e4abb1
Ivan Tranquilan <ivan.tranquilan@outlook.com>
2022-07-26 10:09:44 +0930

    update checkbox

--- react-env-setup/src/App.jsx
+++ react-env-setup/src/App.jsx
@@ -1,16 +1,43 @@
 import './App.css';
 import Todo from './components/todoItem'
 import todosData from './components/todosData';
+import React from 'react';
 
-function App() {
-  
-  const todoItems = todosData.map(elem => <Todo key={elem.id} todo={elem}/>)
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todosData
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
 
-  return (
-    <div className="App">
-      {todoItems}
-    </div>
-  );
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+          console.log(todo.completed)
+        }
+        console.log(todo)
+        return todo
+      })
+      return {
+        todos: updatedTodos
+      }
+    })
+  }
+
+  render() {
+    const todoItems = this.state.todos.map(elem => <Todo key={elem.id} todo={elem}
+    handleChange = {this.handleChange}/>)
+    return(
+      <div className='App'>
+        {todoItems}
+      </div>
+    )
+  }
 }
+
 
 export default App;
--- react-env-setup/src/components/todoItem.jsx
+++ react-env-setup/src/components/todoItem.jsx
@@ -4,12 +4,34 @@ const TodoItem = (props) => {
     return (
         <div>
             <div style={{display: "flex"}}>
-                <input type="checkbox" checked={props.todo.completed}/>
+                <input 
+                    type="checkbox" 
+                    checked={props.todo.completed}
+                    onChange = {() => props.handleChange(props.todo.id)}
+                />
                 <p style={{paddingLeft: 16, maxWidth: 200}}>{props.todo.text}</p>  
             </div>
             <hr />
         </div>
     );
 }
+
+// class TodoItem extends React.Component {
+//     render() {
+//         return(
+//             <div>
+//                 <div style={{display: "flex"}}>
+//                     <input 
+//                         type="checkbox" 
+//                         checked={this.state.completed} 
+//                         onChange={(event) => this.props.handleChange()}
+//                     />
+//                     <p style={{paddingLeft: 16, maxWidth: 200}}>{this.state.text}</p>  
+//                 </div>
+//                 <hr />
+//             </div>
+//         )
+//     }
+// }
 
 export default TodoItem;
