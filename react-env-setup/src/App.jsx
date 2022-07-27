6bc88e0c25ca619688629efa1f3b80a1674b9a7e
Ivan Tranquilan <ivan.tranquilan@outlook.com>
2022-07-27 11:27:05 +0930

    Conditional Rendering Practice

--- react-env-setup/src/App.jsx
+++ react-env-setup/src/App.jsx
@@ -1,38 +1,65 @@
 import './App.css';
-import Todo from './components/todoItem'
-import todosData from './components/todosData';
+// import Todo from './components/todoItem'
+// import todosData from './components/todosData';
 import React from 'react';
+
+// class App extends React.Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       todos: todosData
+//     }
+//     this.handleChange = this.handleChange.bind(this)
+//   }
+
+//   handleChange(id) {
+//     this.setState(prevState => {
+//       const updatedTodos = prevState.todos.map(todo => {
+//         if (todo.id === id) {
+//           todo.completed = !todo.completed
+//           console.log(todo.completed, todo.id, id)
+//         }
+//         return todo
+//       })
+//       return {
+//         todos: updatedTodos
+//       }
+//     })
+//   }
+
+//   render() {
+//     const todoItems = this.state.todos.map(elem => <Todo key={elem.id} todo={elem}
+//     handleChange = {this.handleChange}/>)
+//     return(
+//       <div className='App'>
+//         {todoItems}
+//       </div>
+//     )
+//   }
+// }
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      todos: todosData
+      loggedIn: false
     }
-    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
   }
 
-  handleChange(id) {
+  handleClick() {
     this.setState(prevState => {
-      const updatedTodos = prevState.todos.map(todo => {
-        if (todo.id === id) {
-          todo.completed = !todo.completed
-          console.log(todo.completed, todo.id, id)
-        }
-        return todo
-      })
       return {
-        todos: updatedTodos
+        loggedIn: !prevState.loggedIn
       }
     })
   }
 
   render() {
-    const todoItems = this.state.todos.map(elem => <Todo key={elem.id} todo={elem}
-    handleChange = {this.handleChange}/>)
-    return(
-      <div className='App'>
-        {todoItems}
+    return (
+      <div>
+        <h1>You are currently {this.state.loggedIn ? "Logged in" : "Logged out"}</h1>
+        <button onClick={this.handleClick}>{this.state.loggedIn ? "LOG OUT" : "LOG IN"}</button>
       </div>
     )
   }
