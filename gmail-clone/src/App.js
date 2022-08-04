8446425fedfbb54e911171f8d9a2a0a0772719c3
Ivan Tranquilan <ivan.tranquilan@outlook.com>
2022-08-04 15:30:05 +0930

    header and sidebar completed

--- gmail-clone/src/App.js
+++ gmail-clone/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/header";
+import Sidebar from "./components/sidebar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import EmailList from "./components/EmailList";
+import Mail from "./components/Mail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<EmailList />} />
+            <Route path="/mail" element={<Mail />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ gmail-clone/src/components/EmailList.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./EmailList.css";
+import { Checkbox, IconButton } from "@mui/material";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import RedoIcon from "@mui/icons-material/Redo";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import KeyboardHideIcon from "@mui/icons-material/KeyboardHide";
+import SettingsIcon from "@mui/icons-material/Settings";
+
+const EmailList = () => {
+  return (
+    <div className="email-list">
+      <div className="email-list__settings">
+        <div className="email-list__settings--left">
+          <Checkbox />
+          <IconButton>
+            <ArrowDropDownIcon />
+          </IconButton>
+          <IconButton>
+            <RedoIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+        <div className="email-list__settings--right">
+          <IconButton>
+            <ChevronLeftIcon />
+          </IconButton>
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+          <IconButton>
+            <KeyboardHideIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsIcon />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmailList;
